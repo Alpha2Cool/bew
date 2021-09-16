71bd0763418e8e5ff9700d33db54c351bc74d2b0
Guo Yi <transistors1996@gmail.com>
2021-09-16 23:42:41 +0800

    new change for Field and Constructor

--- src/Reflection/Cat.java
+++ src/Reflection/Cat.java
@@ -2,6 +2,12 @@ package Reflection;
 
 public class Cat {
     private String name = "hello world";
+    public int age = 10;
+
+    public Cat() {}
+    public Cat(String name) {
+        this.name = name;
+    }
 
     public void hi() {
         System.out.println("hi "+name);
--- src/Reflection/factor/ReflectorTest.java
+++ src/Reflection/factor/ReflectorTest.java
@@ -1,13 +1,17 @@
 package Reflection.factor;
 
+import Reflection.Cat;
+
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Properties;
 
 public class ReflectorTest {
-    public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, NoSuchFieldException {
         // 建立Properties对象
         Properties properties = new Properties();
         // 加载指定配置文件
@@ -19,15 +23,30 @@ public class ReflectorTest {
         System.out.println(classfullpath);
         System.out.println(method);
 
-        // 用类路径创建类对象
+        // 用类路径创建类对象 【Class类】
         Class aClass = Class.forName(classfullpath); // 获取对象类
         Object o = aClass.newInstance(); // 获取到对象的实例
 
         System.out.println("o class: "+o.getClass());
 
-        // 调用指定类方法
+        // 调用指定类方法  【Method类】
         Method method1 = aClass.getMethod(method);  // 获取类方法
         method1.invoke(o); // 实现调用
+
+        // 调用成员变量  【Field类】
+        Field age = aClass.getField("age");  // 只能获取公有成员
+        System.out.println(age.get(o));
+
+        // 调用构造函数  【Constructor类】
+        Constructor constructor = aClass.getConstructor();  // 调用无参构造函数
+        System.out.println(constructor);
+        Cat ob = (Cat)constructor.newInstance();
+        ob.hi();
+
+        Constructor constructor1 = aClass.getConstructor(String.class);  // 调用有参构造函数，String.class为Class类的类对象
+        System.out.println(constructor1);
+        Cat ob1 = (Cat)constructor1.newInstance("hhh");
+        ob1.hi();
 
     }
 }
