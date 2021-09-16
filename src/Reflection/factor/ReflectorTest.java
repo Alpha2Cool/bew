5627b1a226e941bf70b7ae297f2d30213a6ea0b0
Guo Yi <transistors1996@gmail.com>
2021-09-17 00:15:29 +0800

    关于反射的优化方案

--- src/Reflection/factor/ReflectorTest.java
+++ src/Reflection/factor/ReflectorTest.java
@@ -23,21 +23,21 @@ public class ReflectorTest {
         System.out.println(classfullpath);
         System.out.println(method);
 
-        // 用类路径创建类对象 【Class类】
+        // 用类路径创建类对象 【java.lang.Class类】
         Class aClass = Class.forName(classfullpath); // 获取对象类
         Object o = aClass.newInstance(); // 获取到对象的实例
 
         System.out.println("o class: "+o.getClass());
 
-        // 调用指定类方法  【Method类】
+        // 调用指定类方法  【java.lang.reflect.Method类】
         Method method1 = aClass.getMethod(method);  // 获取类方法
         method1.invoke(o); // 实现调用
 
-        // 调用成员变量  【Field类】
+        // 调用成员变量  【java.lang.reflect.Field类】
         Field age = aClass.getField("age");  // 只能获取公有成员
         System.out.println(age.get(o));
 
-        // 调用构造函数  【Constructor类】
+        // 调用构造函数  【java.lang.reflect.Constructor类】
         Constructor constructor = aClass.getConstructor();  // 调用无参构造函数
         System.out.println(constructor);
         Cat ob = (Cat)constructor.newInstance();
@@ -47,6 +47,10 @@ public class ReflectorTest {
         System.out.println(constructor1);
         Cat ob1 = (Cat)constructor1.newInstance("hhh");
         ob1.hi();
+
+        // Method类、Field类、Constructor类会继承AccessibleObject类
+        // 如method1.setAccessible(true);  //用于关闭控制访问检测，可加快
+        // method1.invoke(o); 的访问速度，但是提升有限
 
     }
 }
