ab1f7eb3339d084e80e635f45d5b3e32965a7785
coloNN <1030286085@qq.com>
2023-01-30 01:14:39 +0800

    ClassLoader自定义实现

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,17 +1,20 @@
 package test;
 
+import java.io.File;
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        Collections.addAll(list, 4, 3, 2, 1);    //批量add
-        Iterator it = list.iterator();
-        System.out.println(list.toString());
-        Collections.shuffle(list);      //打乱list
-        System.out.println(list.toString());
 
-        Collections.sort(list);   //排序
-        System.out.println(list.toString());
+
+        String s = new String("1,2,3,");
+        System.out.println(s);
+        s=s.substring(0,s.length()-1)+"]";
+        System.out.println(s);
+
+        File file = new File("D:\\", "123.txt");
+        System.out.println(file.getAbsolutePath());
+        file.mkdirs();
+
     }
 }
